914e38bc266fc28f441d1cd51194a898389f0728
Lucas <pennatil@student.ethz.ch>
2012-10-29 17:48:12 +0100

    took away string conversion from rolfs function

--- provadraw_rz.m
+++ provadraw_rz.m
@@ -3,10 +3,8 @@ clc
 A=0;
 A = input(sprintf('How many cars?:',A));
 for i=1:1:A
-%color array LP
+%random color array RZ
 RGB=[rand(1);rand(1);rand(1)]
-%randomizer for the colorLP
-%C=C_temp(floor((rand(1)*7)+1),1);
 %randomize in x position LP_____THE VAR A HAS TO BE MODIFIED ONCE INSERTED IN
 %THE FINAL SCRIPT WITH THE ACTUAL WIDTH
 rand_x=floor(rand(1)*A);
@@ -14,10 +12,10 @@ rand_x=floor(rand(1)*A);
 %THE FINAL SCRIPT WITH THE ACTUAL HEIGHT
 rand_y=floor(rand(1)*A);
 %struct loop LP
-car(i).car_id = num2str(i);
+car(i).car_id = i;
 car(i).color = RGB;
-car(i).pos_x = num2str(rand_x);
-car(i).pos_y = num2str(rand_y);
+car(i).pos_x = rand_x;
+car(i).pos_y = rand_y;
 Provadrawcar_rz(1,1,1,1,RGB,i)
 end
 
