78e0690fe55024d69cac4d4ecc9a38bed6f611f1
Lucas <pennatil@student.ethz.ch>
2012-10-29 14:07:07 +0100

    created struct creator
    this loop creates the cars and puts them in a struct. IT IS NOT FINAL.
    working now on the number id for the struct

--- Car_Struct.m
+++ Car_Struct.m
@@ -1,6 +1,9 @@
 %creating structs car
-
-    car_struct_i = struct ('car_id',i, 'colour_code', 000, 'pos_x',0, 'pos_y',0);
-
-
-            
+clear all
+clc
+A=10;
+for i=1:1:A
+rand_x=floor(rand(1)*A);
+rand_y=floor(rand(1)*A);
+car_struct = struct ('car_id', num2str(i), 'colour_code',  000, 'pos_x', num2str(rand_x), 'pos_y', num2str(rand_y))
+end
