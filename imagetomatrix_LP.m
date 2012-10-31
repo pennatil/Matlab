c2fa4107d29c797c1e5e9d17415bfbcb1c0b190d
Lucas <pennatil@student.ethz.ch>
2012-10-31 16:27:59 +0100

    Script to convert an image to a Matrix NOT FINAL

---
+++ imagetomatrix_LP.m
@@ -0,0 +1,18 @@
+clear all
+clc
+T=imread('rolfmapnl.tif');
+A=single(T(:,:,4));
+clear T;
+S=size(A);
+sx=S(1,1);
+sy=S(1,2);
+for row=1:1:sx
+    for column=1:1:sy
+        if (A(row,column)==0)
+            A(row,column)=1;
+        else
+            A(row,column)=0;
+        end
+    end
+end
+imshow(A,'InitialMagnification','fit')
--- provadraw_rz.m
+++ provadraw_rz.m
@@ -4,7 +4,7 @@ A=0;
 A = input(sprintf('How many cars?:',A));
 for i=1:1:A
 %random color array RZ
-RGB=[rand(1);rand(1);rand(1)]
+RGB=[rand(1);rand(1);rand(1)];
 %randomize in x position LP_____THE VAR A HAS TO BE MODIFIED ONCE INSERTED IN
 %THE FINAL SCRIPT WITH THE ACTUAL WIDTH
 rand_x=floor(rand(1)*A);
