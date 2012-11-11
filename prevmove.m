eb316b5efdbd946fc51426e223ccaace8767c21b
Andrea <annessi@student.ethz.ch>
2012-11-11 15:05:33 +0100

    (forgot 1 file before)

--- prevmove.m
+++ prevmove.m
@@ -3,7 +3,7 @@ if (movement(i-1,j)==0)
     movement(i,j)=0;
 elseif (movement(i-1,j)==2)%if the previous movement was south, next one cannot be north
     %control for another car
-    if (A(B(j,1)+1,B(j,2))==0.3)
+    if (A(B(j,1)+1,B(j,2))~=1 && A(B(j,1)+1,B(j,2))~=-1 && A(B(j,1)+1,B(j,2))~=2)
         movement(i,j)=2;
         %casual crossing (only working in a 4-ways crossing. to improve
         %for a more general situation
@@ -25,7 +25,7 @@ elseif (movement(i-1,j)==2)%if the previous movement was south, next one cannot 
     end
 elseif(movement(i-1,j)==8)%if the previous movement was north, next one cannot be south
     %control for another car
-    if (A(B(j,1)-1,B(j,2))==0.3)
+    if (A(B(j,1)-1,B(j,2))~=1 && A(B(j,1)-1,B(j,2))~=-1)
         movement(i,j)=8;
         %north
     elseif (A(B(j,1)-1,B(j,2))==1)
@@ -42,7 +42,7 @@ elseif(movement(i-1,j)==8)%if the previous movement was north, next one cannot b
     end
 elseif(movement(i-1,j)==6)%if the previous movement was east, next one cannot be west
     %control for another car
-    if (A(B(j,1),B(j,2)+1)==0.3)
+    if (A(B(j,1),B(j,2)+1)~=1 && A(B(j,1),B(j,2)+1)~=-1)
         movement(i,j)=6;
         %east
     elseif (A(B(j,1),B(j,2)+1)==1)
@@ -59,7 +59,7 @@ elseif(movement(i-1,j)==6)%if the previous movement was east, next one cannot be
     end
 elseif(movement(i-1,j)==4)%if the previous movement was west, next one cannot be east
     %control for another car
-    if (A(B(j,1),B(j,2)-1)==0.3)
+    if (A(B(j,1),B(j,2)-1)~=1 && A(B(j,1),B(j,2)-1)~=-1)
         movement(i,j)=4;
         %west
     elseif (A(B(j,1),B(j,2)-1)==1)
