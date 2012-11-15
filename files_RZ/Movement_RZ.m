e792897f4b5dbb3905783da41ab6b51da0718936
Rolf <rolfzu@student.ethz.ch>
2012-11-15 15:46:40 +0100

    car moves with gray
    car moves along gray dot

--- files_RZ/Movement_RZ.m
+++ files_RZ/Movement_RZ.m
@@ -1,8 +1,8 @@
 
 % hight and width def for draw car
 %%
-h=1;
-w=2;
+h=0.5;
+w=1;
 %%
 height=10;
 width=10;
@@ -18,6 +18,10 @@ size(A)
 
 %loop for movements
 for i=2:nIter
+    %set x0, y0 for car patch
+x0=(11-B(1,1))
+y0=B(1,2)
+    
     
     %if south block is empty
 if (A(B(1,1)+1,B(1,2))==1)
@@ -50,8 +54,6 @@ elseif (A(B(1,1),B(1,2)-1)==1)
 end
 %%
 % Draw the car!
-x0=B(1,2);
-y0=B(1,1);
 hold on
 %%
 % Define the coordinates for the car chassi
@@ -90,17 +92,44 @@ imshow(A,'InitialMagnification','fit')
 %%
 % Colour of Car
 C=[0.5 , 0.2,0]%rand(1),rand(1),rand(1)];
+
+%rotation set up
+zdir=[0 0 1];
+
 % patching the car
-patch(x0+chassi_x, y0+chassi_y, C)
-patch(x0+wheel_1_x,y0+wheel_1_y,'k')
-patch(x0+wheel_2_x,y0+wheel_2_y,'k')
-patch(x0+wheel_3_x,y0+wheel_3_y,'k')
-patch(x0+wheel_4_x,y0+wheel_4_y,'k')
-patch(x0+window_1_x,y0+window_1_y,'b')
-patch(x0+window_2_x,y0+window_2_y,C)
-patch(x0+window_3_x,y0+window_3_y,'b')
-patch(x0+window_4_x,y0+window_4_y,'b')
-patch(x0+window_5_x,y0+window_5_y,'b')
+% returning handel h1 -h10 for image rotation
+h1=patch(x0+chassi_x, y0+chassi_y, C);
+rotate(h1,zdir,36.7)
+
+h2=patch(x0+wheel_1_x,y0+wheel_1_y,'k');
+rotate(h2,zdir,36.7)
+
+h3=patch(x0+wheel_2_x,y0+wheel_2_y,'k');
+rotate(h3,zdir,36.7)
+
+h4=patch(x0+wheel_3_x,y0+wheel_3_y,'k');
+rotate(h4,zdir,36.7)
+
+h5=patch(x0+wheel_4_x,y0+wheel_4_y,'k');
+rotate(h5,zdir,36.7)
+
+h6=patch(x0+window_1_x,y0+window_1_y,'b');
+rotate(h6,zdir,36.7)
+
+h7=patch(x0+window_2_x,y0+window_2_y,C);
+rotate(h7,zdir,36.7)
+
+h8=patch(x0+window_3_x,y0+window_3_y,'b');
+rotate(h8,zdir,36.7)
+
+h9=patch(x0+window_4_x,y0+window_4_y,'b');
+rotate(h9,zdir,36.7)
+
+h10=patch(x0+window_5_x,y0+window_5_y,'b');
+rotate(h10,zdir,36.7)
+
+
+
 %%
 
 
