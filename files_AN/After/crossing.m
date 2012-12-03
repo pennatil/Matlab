69a4f03a5f1da97fd5d45daaec0e8746d61038c0
Andrea <annessi@student.ethz.ch>
2012-12-04 00:40:05 +0100

    Added many crossings (3-14) to the after map and fixed the map

---
+++ files_AN/After/crossing.m
@@ -0,0 +1,181 @@
+function [A,B,movement,i,j] = crossing(A,B,movement,i,j)
+if movement(i-1,j)==2
+    if (B(j,1)+1==10 && B(j,2)==56)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==11 && B(j,2)==56)
+        rnd=rand(1);
+        if rnd<=0.5
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=7;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        else
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=7;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        end
+    elseif (B(j,1)+1==12 && B(j,2)==56)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=7;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==13 && B(j,2)==56)
+        rnd=rand(1);
+        if movement(i-3,j)==4
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=7;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        elseif rnd <=0.5
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=7;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        else
+            A(B(j,1),B(j,2)+1)=B(j,3);
+            A(B(j,1),B(j,2))=7;
+            B(j,2)=B(j,2)+1;
+            movement(i,j)=6;
+        end
+    elseif (B(j,1)+1==14 && B(j,2)==56)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    end
+elseif movement(i-1,j)==6
+    if (B(j,1)==6 && B(j,2)+1==4)
+        A(B(j,1),B(j,2)+1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)+1;
+        movement(i,j)=6;
+    elseif (B(j,1)==6 && B(j,2)+1==5)
+        rnd=rand(1);
+        if rnd<=0.3
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        else
+            A(B(j,1),B(j,2)+1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)+1;
+            movement(i,j)=6;
+        end
+    elseif (B(j,1)==6 && B(j,2)+1==6)
+        A(B(j,1),B(j,2)+1)=B(j,3);
+        A(B(j,1),B(j,2))=3;
+        B(j,2)=B(j,2)+1;
+        movement(i,j)=6;
+    elseif (B(j,1)==6 && B(j,2)+1==7)
+        rnd=rand(1);
+        if movement(i-3,j)==2
+            A(B(j,1),B(j,2)+1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)+1;
+            movement(i,j)=6;
+        elseif rnd <=0.5
+            A(B(j,1),B(j,2)+1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)+1;
+            movement(i,j)=6;
+        else
+            A(B(j,1)-1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)-1;
+            movement(i,j)=8;
+        end
+    end
+elseif movement(i-1,j)==8
+    if (B(j,1)-1==6 && B(j,2)==6)
+        A(B(j,1)-1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)-1;
+        movement(i,j)=8;
+    elseif (B(j,1)-1==5 && B(j,2)==6)
+        rnd=rand(1);
+        if rnd<=0.3
+            A(B(j,1),B(j,2)+1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)+1;
+            movement(i,j)=6;
+        else
+            A(B(j,1)-1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)-1;
+            movement(i,j)=8;
+        end
+    elseif (B(j,1)-1==4 && B(j,2)==6)
+        A(B(j,1)-1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=3;
+        B(j,1)=B(j,1)-1;
+        movement(i,j)=8;
+    elseif (B(j,1)-1==3 && B(j,2)==6)
+        rnd=rand(1);
+        if movement(i-3,j)==6
+            A(B(j,1)-1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)-1;
+            movement(i,j)=8;
+        elseif rnd <=0.5
+            A(B(j,1)-1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)-1;
+            movement(i,j)=8;
+        else
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        end
+    end
+elseif movement(i-1,j)==4
+    if (B(j,1)==4 && B(j,2)-1==6)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==4 && B(j,2)-1==5)
+        rnd=rand(1);
+        if rnd<=0.3
+            A(B(j,1)-1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)-1;
+            movement(i,j)=8;
+        else
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        end
+    elseif (B(j,1)==4 && B(j,2)-1==4)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=3;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==4 && B(j,2)-1==3)
+        rnd=rand(1);
+        if movement(i-3,j)==8
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        elseif rnd <=0.5
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        else
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=3;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        end
+    end
+    
+end
+end
---
+++ files_AN/After/crossing_13.m
@@ -0,0 +1,152 @@
+function [A,B,movement,i,j] = crossing_13(A,B,movement,i,j)
+if movement(i-1,j)==2
+    if (B(j,1)+1==105 && B(j,2)==40)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==106 && B(j,2)==40)
+        rnd=rand(1);
+        if rnd<=0.3
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        else
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        end
+    elseif (B(j,1)+1==107 && B(j,2)==40)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==108 && B(j,2)==40)
+        rnd=rand(1);
+        if rnd <=0.25
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        else
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        end
+    elseif (B(j,1)+1==109 && B(j,2)==40)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==110 && B(j,2)==40)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==111 && B(j,2)==40)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==105 && B(j,2)==42)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==106 && B(j,2)==42)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==107 && B(j,2)==42)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==108 && B(j,2)==42)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==109 && B(j,2)==42)
+        A(B(j,1)+1,B(j,2))=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,1)=B(j,1)+1;
+        movement(i,j)=2;
+    elseif (B(j,1)+1==110 && B(j,2)==42)
+        A(B(j,1),B(j,2)+1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)+1;
+        movement(i,j)=6;
+    end
+elseif movement(i-1,j)==4
+    if (B(j,1)==105 && B(j,2)-1==42)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==105 && B(j,2)-1==41)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==105 && B(j,2)-1==40)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==105 && B(j,2)-1==39)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==105 && B(j,2)-1==38)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==107 && B(j,2)-1==42)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==107 && B(j,2)-1==41)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==107 && B(j,2)-1==40)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=13;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    elseif (B(j,1)==107 && B(j,2)-1==39)
+        rnd=rand(1);
+        if rnd <=0.5
+            A(B(j,1),B(j,2)-1)=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,2)=B(j,2)-1;
+            movement(i,j)=4;
+        else
+            A(B(j,1)+1,B(j,2))=B(j,3);
+            A(B(j,1),B(j,2))=13;
+            B(j,1)=B(j,1)+1;
+            movement(i,j)=2;
+        end
+    elseif (B(j,1)==107 && B(j,2)-1==38)
+        A(B(j,1),B(j,2)-1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)-1;
+        movement(i,j)=4;
+    end
+elseif movement(i-1,j)==6
+    if (B(j,1)==109 && B(j,2)+1==44)
+        A(B(j,1),B(j,2)+1)=B(j,3);
+        A(B(j,1),B(j,2))=1;
+        B(j,2)=B(j,2)+1;
+        movement(i,j)=6;
+end
+end
