53475b222e9d2196e7684193a71ec18326375722
Andrea <annessi@student.ethz.ch>
2012-12-09 22:35:23 +0100

    forgot a ; -.-

--- MAIN/Before PVP/prevmove.m
+++ MAIN/Before PVP/prevmove.m
@@ -1,6 +1,6 @@
 function [A,B,movement,i,j,nCarsOut] = prevmove(A,B,movement,i,j,nCarsOut)
 %Probability of making movement 5 AN
-m=0.5
+m=0.5;
 
 if (movement(i-1,j)==5)
     movement(i,j)=movement(i-2,j);
